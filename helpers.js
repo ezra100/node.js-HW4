5c6a71bcc63524ab27aa853d01d4b09da76d03b3
ezra100 <ezra250@gmail.com>
2018-01-01 15:10:19 +0200

    moved functions to a seperate helpers file

---
+++ helpers.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const types_1 = require("./types");
+var helpers;
+(function (helpers) {
+    function objectToUser(obj) {
+        if (obj.gender && typeof obj.gender !== "number") {
+            obj.gender = parseInt(obj.gender, 10);
+        }
+        switch (obj.className.toLowerCase()) {
+            case "user":
+                return new types_1.User(obj);
+            case "manager":
+                return new types_1.Manager(obj);
+            case "myworker":
+                return new types_1.MyWorker(obj);
+            case "customer":
+                return new types_1.Customer(obj);
+        }
+    }
+    helpers.objectToUser = objectToUser;
+    function dataFilter(users, filter) {
+        if (filter.gender && typeof filter.gender !== "number") {
+            filter.gender = parseInt(filter.gender, 10);
+        }
+        return users.filter((user) => {
+            for (var key in filter) {
+                if (filter[key] && filter[key] !== "" && filter[key] !== user[key]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
+    helpers.dataFilter = dataFilter;
+})(helpers = exports.helpers || (exports.helpers = {}));
+//# sourceMappingURL=helpers.js.map
---
+++ helpers.ts
@@ -0,0 +1,33 @@
+import { User, Manager, MyWorker, Customer } from "./types";
+
+export namespace helpers {
+    export function objectToUser(obj: any): User {
+        if (obj.gender && typeof obj.gender !== "number") {
+            obj.gender = parseInt(obj.gender, 10);
+        }
+        switch (obj.className.toLowerCase()) {
+            case "user":
+                return new User(obj);
+            case "manager":
+                return new Manager(obj);
+            case "myworker":
+                return new MyWorker(obj);
+            case "customer":
+                return new Customer(obj);
+        }
+    }
+
+    export function dataFilter(users: User[], filter: any): User[] {
+        if (filter.gender && typeof filter.gender !== "number") {
+            filter.gender = parseInt(filter.gender, 10);
+        }
+        return users.filter((user) => {
+            for (var key in filter) {
+                if (filter[key] && filter[key] !== "" && filter[key] !== (<any>user)[key]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
+}
