cb794aeb8d2cb0650793f151c0bd471d03183b5b
ezra250@gmail.com <ezra250@gmail.com>
2018-03-28 17:15:53 +0300

    Full switch of DB to mongodb

--- DataBase/mongoDB.ts
+++ DataBase/mongoDB.ts
@@ -9,17 +9,19 @@ import { resolve } from "dns";
 
 
 // set up default mongoose connection
-var connectionString: string = "mongodb://127.0.0.1/localhost";
+var connectionString: string = "mongodb://127.0.0.1/flowersPP";
 mongoose.connect(connectionString);
 
 // get the default connection
 var db: mongoose.Connection = mongoose.connection;
+
+//#region schema
 // define a schema
-var Schema: any = mongoose.Schema;
+let Schema: any = mongoose.Schema;
 
 
 
-var UserSchema: any = new Schema({
+let UserSchema: any = new Schema({
     _id: String,
     className: String,
     firstName: String,
@@ -40,6 +42,26 @@ let employeerModel: mongoose.Model<any> = userModel.discriminator("Employee",
 let managerModel: mongoose.Model<any> = userModel.discriminator("Manager",
     new mongoose.Schema({ branchID: Number }, { discriminatorKey: "manager" }));
 
+let branchSchema: any = new Schema({
+    _id: Number,
+    id: Number,
+    address: String,
+    active: Boolean,
+    name: String
+});
+let branchModel: mongoose.Model<any> = mongoose.model("Branch", branchSchema);
+let flowerSchema: any = new Schema({
+    _id : String,
+    name: String,
+    family: String,
+    price: Number,
+    img: String,
+    colorDesc: String,
+    color: String,
+});
+let flowerModel: mongoose.Model<any> = mongoose.model("Flower", flowerSchema);
+//#endregion
+
 function init(): void {
     users
         .map((u) => new userModel(Object.assign({}, u, { _id: u.userName })))
@@ -53,13 +75,76 @@ function init(): void {
         ));
 }
 init();
+
+
 // bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 export class MongoDB {
 
-    getFlowers(): Flower[] {
-        return flowers;
+    getFlowers(): Promise<Flower[]> {
+        return new Promise<Flower[]>(
+            (resolve, reject) => {
+                flowerModel.find({}, (err: Error, flowers: Flower[]) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(flowers);
+                });
+            }
+        );
+    }
+
+    findFlower(name: string): Promise<Flower | null> {
+        return new Promise<Flower | null>(
+            (resolve, reject) => {
+                flowerModel.findById(name, (err: Error, flower: Flower) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(flower);
+                });
+            }
+        );
+    }
+    updateFlower(flower: Flower): Promise<Flower | null> {
+        return new Promise((resolve, reject) => {
+            flowerModel.findByIdAndUpdate(flower.name, flower, (err: Error, oldFlower: Flower) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                // we want to send back the new one
+                resolve(flower);
+            });
+        });
+    }
+    addFlower(flower: Flower): Promise<Flower | null> {
+        return new Promise(
+            (resolve, reject) => {
+                let flowerDoc: any = new flowerModel(Object.assign({}, flower, { _id: flower.name }));
+                flowerDoc.save((err: Error, flower: Flower) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(flower);
+                });
+            }
+        );
+    }
+    deleteFlower(flower: Flower): Promise<Flower | null> {
+        return new Promise((resolve, reject) => {
+            flowerModel.findByIdAndRemove(flower.name, (err: Error, flower: Flower) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(flower);
+            });
+        });
     }
     //#region users
     // todo: return the users as a user object(?)
@@ -170,66 +255,110 @@ export class MongoDB {
     //#endregion
 
     //#region branches
-    getBranches(filter: any): Branch[] {
-        return branches.filter((branch) => {
+    getBranches(filter: any): Promise<Branch[]> {
+        for (const key of Object.keys(filter)) {
+            if (filter[key] === "") {
+                delete filter[key];
+                continue;
+            }
+            switch (getBranchKeyType(key)) {
+                case "string":
+                    // replace it with a regex that will search for any one of the given words
+                    filter[key] = new RegExp(filter[key].split(/\s+/)
+                        // escape regex characters
+                        .map((v: any) => v.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+                        ).join("|"), "gi");
+                    break;
+                case "boolean":
 
-            for (var key in filter) {
-                if (filter[key] === "") {
-                    continue;
-                }
-                switch (getUserKeyType(key)) {
-                    case "string":
-                        var regex: RegExp = new RegExp(filter[key].split(/\s+/).join("|"), "gi");
-                        if (!regex.test((<any>branch)[key])) {
-                            return false;
-                        }
-                        break;
-                    case "boolean":
-                        if (typeof filter[key] === "string") {
+                    if (typeof filter[key] === "string") {
+                        if (filter[key] === "") {
+                            delete filter[key];
+                        } else {
                             filter[key] = (filter[key] === "true");
                         }
-                        if (filter[key] !== (<any>branch)[key]) {
-                            return false;
-                        }
-                        break;
-                    case "number":
-                        if (parseFloat(filter[key]) !== (<any>branch)[key]) {
-                            return false;
-                        }
-                }
+                    }
+                    break;
+                case "number":
+                    // exception for 'gender' since it's an enum
+                    if (typeof filter[key] === "string" || filter[key] instanceof String) {
+                        filter[key] = parseInt(filter[key], 10);
+                    }
+                    if (key === "gender" && filter[key] === 0) {
+                        delete filter[key];
+                    }
             }
-            return true;
+        }
+        return new Promise<Branch[]>(
+            (resolve, reject) => {
+                userModel.find(filter, (err: Error, branches: Branch[]) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(branches);
+                });
+            }
+        );
+
+    }
+    findBranch(id: string): Promise<Branch | null> {
+        return new Promise<Branch | null>(
+            (resolve, reject) => {
+                branchModel.findById(id, (err: Error, branch: Branch) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(branch);
+                });
+            }
+        );
+    }
+    updateBranch(branch: Branch): Promise<Branch | null> {
+        return new Promise((resolve, reject) => {
+            branchModel.findByIdAndUpdate(branch.id, branch, (err: Error, oldBranch: Branch) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                // we want to send back the new one
+                resolve(branch);
+            });
         });
     }
-    addBranch(branch: Branch): Branch {
-        if (branches.findIndex((b) => branch.id === b.id) >= 0) {
-            console.error("User" + branch.id + ":" + branch.name + " already exists");
-            return null;
-        }
-        branches.push(branch);
-        return branch;
+    addBranch(branch: Branch): Promise<Branch | null> {
+        return new Promise(
+            (resolve, reject) => {
+                let branchDoc: any = new branchModel(Object.assign({}, branch, { _id: branch.id }));
+                branchDoc.save((err: Error, branch: Branch) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(branch);
+                });
+            }
+        );
     }
-    updateBranch(branch: Branch): Branch {
-        var index: number = branches.findIndex((b: Branch) => b.id === branch.id);
-        if (index < 0) {
-            console.error("Branch not found");
-            return null;
-        }
-        branches[index] = branch;
-        return branch;
-    }
-    deleteBranch(branch: Branch): Branch {
-        var index: number = branches.findIndex((b: Branch) => b.id === branch.id);
-        if (index < 0) {
-            console.error("Branch not found");
-            return null;
-        }
-        branches.splice(index, 1);
-        return branch;
+    deleteBranch(branch: Branch): Promise<Branch | null> {
+        return new Promise((resolve, reject) => {
+            branchModel.findByIdAndRemove(branch.id, (err: Error, branch: Branch) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(branch);
+            });
+        });
     }
     //#endregion
 }
 
 function getUserKeyType(key: string): string {
     return (<string>(<any>userModel.schema).paths[key].instance).toLowerCase();
+}
+
+function getBranchKeyType(key: string): string {
+    return (<string>(<any>branchModel.schema).paths[key].instance).toLowerCase();
 }
